83efa139edf9fdf094f6dbb12bec09cd4a1dc90e
Morrison Cole <admin@morrisoncole.co.uk>
2019-08-01 10:19:18 +0900

    Add matchers, run tests with Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,13 @@ repositories {
 
 dependencies {
     testCompile("org.junit.jupiter", "junit-jupiter-engine", "5.5.1")
+    testCompile("org.hamcrest", "hamcrest", "2.1")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
